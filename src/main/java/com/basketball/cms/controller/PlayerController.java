f51463afef22a4a035420f8aaea938f916f30198
laiminhan <laiminhan@localhost>
2024-05-17 16:24:00 +0800

    commit4

--- src/main/java/com/basketball/cms/controller/PlayerController.java
+++ src/main/java/com/basketball/cms/controller/PlayerController.java
@@ -228,11 +228,6 @@ public class PlayerController {
         positions.add("GUARD");
         positions.add("FORWARD");
         positions.add("CENTER");
-        //positions.add("GUARD-FORWARD");
-        //positions.add("CENTER-FORWARD");
-//        positions.add("FORWARD-CENTER");
-//        positions.add("FORWARD-GUARD");
-//        positions.add("GUARD-FORWARD");
         return positions;
     }
 
@@ -247,17 +242,17 @@ public class PlayerController {
         return countries;
     }
 
-    @ModelAttribute("allInjuries")
-    public List<Integer> allInjuries() {
-        //String[]injury = {"0 - No Injuries", "1 - Ankle Sprains", "2 - Facial Cuts", "3 - Knee Injuries", "4 - Jammed Fingers", "5 - Calf Strains / Achilles Tears", "6 - Thigh Bruises"};
-        Integer[] injury = {0, 1, 2, 3, 4, 5, 6};
-        //List<String>countries = new ArrayList<>(Arrays.asList(cc));
-        List<Integer> injuries = new ArrayList<>();
-        for (int i = 0; i < injury.length; i++) {
-            injuries.add(injury[i]);
-        }
-        return injuries;
-    }
+//    @ModelAttribute("allInjuries") delete injury in search page
+//    public List<Integer> allInjuries() {
+//        //String[]injury = {"0 - No Injuries", "1 - Ankle Sprains", "2 - Facial Cuts", "3 - Knee Injuries", "4 - Jammed Fingers", "5 - Calf Strains / Achilles Tears", "6 - Thigh Bruises"};
+//        Integer[] injury = {0, 1, 2, 3, 4, 5, 6};
+//        //List<String>countries = new ArrayList<>(Arrays.asList(cc));
+//        List<Integer> injuries = new ArrayList<>();
+//        for (int i = 0; i < injury.length; i++) {
+//            injuries.add(injury[i]);
+//        }
+//        return injuries;
+//    }
 
     @GetMapping("/sort")
 //    public String sortPlayersByOverallScore(@RequestParam(required = false, defaultValue = "asc") String order, Model model) {
