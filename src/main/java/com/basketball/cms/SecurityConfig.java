b9e0f9b43a6ccf5047eca72aa0dfca0fd65860d8
Ooiky0625 <kwongyang78@gmail.com>
2024-05-24 20:51:18 +0800

    comment added in SecurityConfig(Login Page)

--- src/main/java/com/basketball/cms/SecurityConfig.java
+++ src/main/java/com/basketball/cms/SecurityConfig.java
@@ -20,34 +20,41 @@ import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 public class SecurityConfig {
-
+    
+    //Configure an in-memory user details service with a predefined user
     @Bean
     UserDetailsService userDetailsService() {
         InMemoryUserDetailsManager userDetailsService = new InMemoryUserDetailsManager();
+        // Creates a user with username "HokkienMee" and password "HokkienMeeIsRed2024"
         UserDetails user = User.withUsername("HokkienMee").password(passwordEncoder().encode("HokkienMeeIsRed2024"))
                 .authorities("read").build();
         userDetailsService.createUser(user);
         return userDetailsService;
     }
-
+    
+    //Configure a BCrypt password encoder
     @Bean
     BCryptPasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
     }
-
+    
+    //Configure the security filter chain
     @Bean
     SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http
-                .csrf().disable()
+                .csrf().disable()// Disables CSRF protection
                 .authorizeRequests()
+                // Allows unrestricted access to the root, players API, locations, and player profiles
                 .requestMatchers("/").permitAll() 
                 .requestMatchers("/players/api/**").permitAll()
                 .requestMatchers("/locations/**").permitAll()
                 .requestMatchers("/players/profile/**").permitAll()
+                // Requires authentication for all other player-related and team-related endpoints
                 .requestMatchers("/players/**").authenticated()
                 .requestMatchers("/team/**").authenticated()
                 .and()
                 .formLogin()
+                // Specifies the login page URL and the default success URL after login
                 .loginPage("/login") 
                 .defaultSuccessUrl("/players", true)
                 .permitAll();
